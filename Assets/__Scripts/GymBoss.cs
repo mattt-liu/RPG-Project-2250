920c31014fd6e076bb3922ed0e42111402d26468
Matthew Liu <mattliuusa@hotmail.com>
2020-03-18 15:38:02 -0400

    Revert "Revert "added enemy""
    This reverts commit de075e1d920b25fda781b4cf699ea3f68d1b46b2.

---
+++ Assets/__Scripts/GymBoss.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gymboss : Inter
+{
+
+    public override void Interact()
+    {
+        base.Interact();
+    }
+}
