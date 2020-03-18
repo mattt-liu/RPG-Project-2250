65338aefa4ac129989eb44d4e9457a8175622181
Matthew Liu <mattliuusa@hotmail.com>
2020-03-17 23:52:24 -0400

    Revert "Merge branch 'master' of https://github.com/mattt-liu/RPG-Project-2250"
    This reverts commit 7ae9b18790d95142a3068add43a388c312519846, reversing
    changes made to d3cafc8994a1387611a4b7cb7fc6130012ba3082.

--- Assets/__Scripts/PlayerController.cs
+++ Assets/__Scripts/PlayerController.cs
@@ -5,59 +5,33 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerMovement))]
 public class PlayerController : MonoBehaviour
 {
+
     public LayerMask mask;
     public Camera cam;
     public PlayerMovement mover;
-
     public Inter focus;
-
-
-    public PlayerAnimation animate;
-
-    private Vector3 _curPos;
-    private Vector3 _newPos;
-    private bool _moving;
 
 
     void Start()
     {
-        _curPos = transform.position;
-        _newPos = transform.position;
         cam = Camera.main;
         mover = GetComponent<PlayerMovement>();
     }
 
     void Update()
     {
-        _curPos = transform.position;
-
-        // Moving player
-
-        if (Input.GetMouseButtonDown(0))
-            if (Input.GetMouseButtonDown(1))
-            {
-
-                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit, 100, mask))
-                {
-                    _newPos = hit.point;
-                    mover.MoveToPoint(_newPos);
-                }
-            }
-
-        // Checking for movement
-
-        _moving = (_newPos.x != _curPos.x) && (_newPos.z != _curPos.z);
-
-    }
-    void LateUpdate()
-    {
-
-        if (_moving)
+        if (Input.GetMouseButtonDown(1))
         {
-            animate.Move();
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, mask))
+            {
+                mover.MoveToPoint(hit.point);
+                // Move player
+
+                // Stop focusing
+            }
         }
         // If we press right mouse
         if (Input.GetMouseButtonDown(0))
