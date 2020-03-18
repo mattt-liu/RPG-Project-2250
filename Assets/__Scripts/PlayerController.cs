d3cafc8994a1387611a4b7cb7fc6130012ba3082
Seyon24 <60857308+Seyon24@users.noreply.github.com>
2020-03-17 23:34:39 -0400

    changed script and gym

--- Assets/__Scripts/PlayerController.cs
+++ Assets/__Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public LayerMask mask;
     public Camera cam;
     public PlayerMovement mover;
+    public Inter focus;
+
 
     void Start()
     {
@@ -31,5 +33,49 @@ public class PlayerController : MonoBehaviour
                 // Stop focusing
             }
         }
+        // If we press right mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            // We create a ray
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // If the ray hits
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Inter interactable = hit.collider.GetComponent<Inter>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+
+        }
+}
+    // Set our focus to a new focus
+    void SetFocus(Inter newFocus)
+    {
+        // If our focus has changed
+        if (newFocus != focus)
+        {
+            // Defocus the old one
+            if (focus != null)
+                focus.OnDefocused();
+
+            focus = newFocus;   // Set our new focus
+            mover.Follow(newFocus);   // Follow the new focus
+        }
+
+        newFocus.OnFocused(transform);
+    }
+
+    // Remove our current focus
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+        mover.StopFollowing();
     }
 }
