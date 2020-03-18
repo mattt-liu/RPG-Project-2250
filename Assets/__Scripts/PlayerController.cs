9682be291e1a4c51caad15e420f26801e430884a
Matthew Liu <mattliuusa@hotmail.com>
2020-03-18 00:07:45 -0400

    Revert "revert "added scripts""
    This reverts commit 91e731d21f0f171e38d86e194d1c4832bd2f17ab.

--- Assets/__Scripts/PlayerController.cs
+++ Assets/__Scripts/PlayerController.cs
@@ -8,12 +8,9 @@ public class PlayerController : MonoBehaviour
     public LayerMask mask;
     public Camera cam;
     public PlayerMovement mover;
-<<<<<<< HEAD
 
     public Inter focus;
 
-=======
->>>>>>> parent of d3cafc8... changed script and gym
 
     public PlayerAnimation animate;
 
@@ -62,5 +59,49 @@ public class PlayerController : MonoBehaviour
         {
             animate.Move();
         }
+        // If we press right mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            // We create a ray
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // If the ray hits
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Inter interactable = hit.collider.GetComponent<Inter>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+
+        }
+}
+    // Set our focus to a new focus
+    void SetFocus(Inter newFocus)
+    {
+        // If our focus has changed
+        if (newFocus != focus)
+        {
+            // Defocus the old one
+            if (focus != null)
+                focus.OnDefocused();
+
+            focus = newFocus;   // Set our new focus
+            mover.Follow(newFocus);   // Follow the new focus
+        }
+
+        newFocus.OnFocused(transform);
+    }
+
+    // Remove our current focus
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+        mover.StopFollowing();
     }
 }
