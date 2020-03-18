b57fca4b5a714425cb9c4700ed49582b19cfac90
Matthew Liu <mattliuusa@hotmail.com>
2020-03-17 23:53:50 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/mattt-liu/RPG-Project-2250""
    This reverts commit 65338aefa4ac129989eb44d4e9457a8175622181.

--- Assets/__Scripts/PlayerController.cs
+++ Assets/__Scripts/PlayerController.cs
@@ -5,33 +5,59 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerMovement))]
 public class PlayerController : MonoBehaviour
 {
-
     public LayerMask mask;
     public Camera cam;
     public PlayerMovement mover;
+
     public Inter focus;
+
+
+    public PlayerAnimation animate;
+
+    private Vector3 _curPos;
+    private Vector3 _newPos;
+    private bool _moving;
 
 
     void Start()
     {
+        _curPos = transform.position;
+        _newPos = transform.position;
         cam = Camera.main;
         mover = GetComponent<PlayerMovement>();
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        _curPos = transform.position;
 
-            if (Physics.Raycast(ray, out hit, 100, mask))
+        // Moving player
+
+        if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(1))
             {
-                mover.MoveToPoint(hit.point);
-                // Move player
 
-                // Stop focusing
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100, mask))
+                {
+                    _newPos = hit.point;
+                    mover.MoveToPoint(_newPos);
+                }
             }
+
+        // Checking for movement
+
+        _moving = (_newPos.x != _curPos.x) && (_newPos.z != _curPos.z);
+
+    }
+    void LateUpdate()
+    {
+
+        if (_moving)
+        {
+            animate.Move();
         }
         // If we press right mouse
         if (Input.GetMouseButtonDown(0))
