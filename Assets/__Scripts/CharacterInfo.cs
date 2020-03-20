b7b196a01cd6ccc31b32f88b85e728e820ddb93e
Marcus Del Vecchio <marcusdel2112@rogers.com>
2020-03-20 01:29:25 -0400

    Character Customization

---
+++ Assets/__Scripts/CharacterInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfo : MonoBehaviour
+{
+    public string CharacterType;
+
+    [HideInInspector]
+    public string Name;
+
+    public int health;
+    public int strength;
+    public int speed;
+}
