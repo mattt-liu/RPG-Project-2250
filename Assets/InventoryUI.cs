b3a4b107c055f4e845c4273d1eb48a7798e570af
Matthew Liu <mattliuusa@hotmail.com>
2020-03-20 02:44:18 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/mattt-liu/RPG-Project-2250""
    This reverts commit c74bbc2b868f8f1fcb322dae5b7fd2ff60abbd83.

---
+++ Assets/InventoryUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    Inventory inventory;
+    void Start()
+    {
+       inventory = Inventory.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
